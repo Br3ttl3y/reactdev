27d046b0b0969c1d1886d50190f07d8ee730de11
Brett <therealbrettcarroll@pm.me>
2024-07-26 10:33:01 -0700

    Added move label
    Part of the tutorial made me add a label for the current move instead of a button.

--- src/App.js
+++ src/App.js
@@ -88,6 +88,7 @@ export default function Game() {
       </div>
       <div className="game-info">
         <ol>{moves}</ol>
+        You are at move #{(currentMove + 1)}
       </div>
     </div>
   );
